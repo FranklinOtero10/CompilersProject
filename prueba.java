2fcc5981a89cf8904386804e89567ca14f571473
Danni96 <loddanny9@gmail.com>
2023-11-29 09:12:33 -0600

    agregando java

--- prueba.java
+++ prueba.java
@@ -2,9 +2,12 @@ int num = 5;
 String c = "hola mundo";
 boolean esVerdadero = true;
 
+int producto = 1; 
+
 for (int i = 0; i < 5; i++) {
-    System.out.println("Iteracion " + (i + 1));
+    producto *= (i + 1); 
 }
+System.out.println("El producto total es: " + producto); 
 
 if (esVerdadero) {
     System.out.println("La condicion es verdadera");
